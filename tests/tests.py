d17b370af1462b9b6d18342e96094cd339aeb797
Oleksii Manzik <oleksii.manzik@gmail.com>
2019-10-04 09:30:54 +0300

    formatted sum_of_corners and radius_of_inscribed_circle tests

--- tests/tests.py
+++ tests/tests.py
@@ -20,15 +20,16 @@ class RectangleTestCase(unittest.TestCase):
         self.assertEqual(self.test_rectangle_3.get_rectangle_square(), 25)
 
     def test_sum_of_corners_calc(self):
-        for i in range(1, 10):
+        for i in range(1, 5):
             with self.subTest(name=f'sum of {i} corners'):
-                if i <= 4:
-                    self.assertEqual(
-                        self.test_rectangle_1.get_sum_of_corners(i),
-                        i * 90)
-                else:
-                    with self.assertRaises(ValueError):
-                        self.test_rectangle_1.get_sum_of_corners(i)
+                self.assertEqual(
+                    self.test_rectangle_1.get_sum_of_corners(i), i * 90)
+
+    def test_sum_of_corners_error_raise(self):
+        for i in range(5, 10):
+            with self.subTest(name=f'sum of {i} corners'):
+                self.assertRaises(ValueError,
+                                  self.test_rectangle_1.get_sum_of_corners, i)
 
     def test_rectangle_diagonal_calc(self):
         self.assertEqual(self.test_rectangle_1.get_rectangle_diagonal(),
@@ -50,18 +51,13 @@ class RectangleTestCase(unittest.TestCase):
             math.sqrt(50) / 2)
 
     def test_radius_of_inscribed_circle_calc(self):
-        with self.subTest(name='test_rectangle_1'):
-            with self.assertRaises(ValueError):
-                self.test_rectangle_1.get_radius_of_inscribed_circle()
-
-        with self.subTest(name='test_rectangle_2'):
-            with self.assertRaises(ValueError):
-                self.test_rectangle_2.get_radius_of_inscribed_circle()
-
-        with self.subTest(name='test_rectangle_3'):
-            self.assertEqual(
-                self.test_rectangle_3.get_radius_of_inscribed_circle(),
-                math.sqrt(50) / (2 * math.sqrt(2)))
+        self.assertRaises(ValueError,
+                          self.test_rectangle_1.get_radius_of_inscribed_circle)
+        self.assertRaises(ValueError,
+                          self.test_rectangle_2.get_radius_of_inscribed_circle)
+        self.assertEqual(
+            self.test_rectangle_3.get_radius_of_inscribed_circle(),
+            math.sqrt(50) / (2 * math.sqrt(2)))
 
 
 if __name__ == '__main__':
